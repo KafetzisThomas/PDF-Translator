c7ddaff6c5d00d3de5ffaafc4f084a0ee33b1da2
KafetzisThomas <tomkafetzis06@gmail.com>
2025-02-14 01:01:21 +0200

    Translate the text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import nltk
 import pymupdf
-from nltk.tokenize import PunktTokenizer
+from nltk.tokenize import sent_tokenize
+from transformers import MarianMTModel, MarianTokenizer
 
 nltk.download("punkt")
 nltk.download("punkt_tab")
@@ -11,17 +12,35 @@ result = ""
 for page in doc:
     result += page.get_text()
 
-print(result[:1000])
+print("Original Text:\n")
+print(result)
 
 
 def split_text(text):
     """
     Split into sentences.
     """
-    sent_detector = PunktTokenizer()
-    return sent_detector.tokenize(text.strip())
+    return sent_tokenize(text.strip())
 
 
-print("\n")
-print(split_text(result[:1000]))
-print(type(split_text(result[:1000])))
+chunks = split_text(result)
+# print(type(chunks))
+
+
+model_name = "Helsinki-NLP/opus-mt-en-el"  # for specific language translation
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+
+
+def translate_text(sentences, batch_size=5):
+    translations = []
+    for i in range(0, len(sentences), batch_size):
+        batch = sentences[i : i + batch_size]
+        inputs = tokenizer(batch, return_tensors="pt", padding=True, truncation=True)
+        outputs = model.generate(**inputs)
+        translations.extend(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+    return translations
+
+
+print("Translated Text:\n")
+print(translate_text(chunks))
