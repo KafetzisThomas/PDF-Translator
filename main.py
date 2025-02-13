f1c42642dea0c585c55537eaea6d282211efb2d4
KafetzisThomas <tomkafetzis06@gmail.com>
2025-02-14 00:20:49 +0200

    Split text into chunks

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import nltk
 import pymupdf
+from nltk.tokenize import PunktTokenizer
 
-doc = pymupdf.open("test.pdf")
+nltk.download("punkt")
+nltk.download("punkt_tab")
+
+doc = pymupdf.open("media/test.pdf")
 
 result = ""
 for page in doc:
     result += page.get_text()
 
 print(result[:1000])
+
+
+def split_text(text):
+    """
+    Split into sentences.
+    """
+    sent_detector = PunktTokenizer()
+    return sent_detector.tokenize(text.strip())
+
+
+print("\n")
+print(split_text(result[:1000]))
+print(type(split_text(result[:1000])))
